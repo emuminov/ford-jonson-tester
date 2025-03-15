076ba190518410a18c8cd74a0a6482c918f9be15
emuminov <jseldardw@gmail.com>
2025-03-15 18:39:24 +0100

    feat: made the tester much-much fater by removing grep subprocessing, adding PrcessPoolExecutor and parallelizing subprocesses

--- tester.py
+++ tester.py
@@ -1,9 +1,17 @@
 import argparse
 import os
 import pathlib
+import sys
 import random
+import re
 import subprocess
+from concurrent.futures import ProcessPoolExecutor, as_completed
 from math import ceil, log2
+
+
+class ErrorCode:
+    RETURNCODE_ERROR = -1
+    NO_NUMBER_OF_COMPARISONS = -2
 
 
 def calculate_number_of_maximal_comparisons(n: int):
@@ -30,6 +38,25 @@ def create_test_input(start: int, end: int):
     return [str(i) for i in res]
 
 
+def run_test(executable_path, test_input):
+    result = subprocess.run(
+        [executable_path] + test_input,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True,
+    )
+
+    inputs = result.args[1:]
+    if result.returncode != 0:
+        return ErrorCode.RETURNCODE_ERROR, inputs, result.returncode
+
+    found_pattern = re.search(r"^Number of comparisons: [0-9]+$", result.stdout, re.MULTILINE)
+    if not found_pattern:
+        return ErrorCode.NO_NUMBER_OF_COMPARISONS, inputs, result.returncode
+
+    return int(found_pattern.group(0).split()[3]), inputs, result.returncode
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Tests the Ford-Johnson algorithm.")
     parser.add_argument(
@@ -41,25 +68,33 @@ if __name__ == "__main__":
         help="Path to the PmergeMe executable to test.",
     )
     args = parser.parse_args()
-    testing_sets = (1, 21), (1, 50), (1, 100), (1, 1000)
+    default_testing_sets = (1, 21), (1, 50), (1, 100), (1, 1000)
 
-    for test_set in testing_sets:
+    for test_set in default_testing_sets:
         print(f"Testing set of {test_set[1]} numbers:")
+        test_inputs = [create_test_input(*test_set) for _ in range(1000)]
         results = []
-        for i in range(1, 10001):
-            test_input = create_test_input(*test_set)
-            # TODO: check the exit code of the program
-            result = subprocess.run([args.executable.absolute()] + test_input, stdout=subprocess.PIPE)
+        with ProcessPoolExecutor() as executor:
+            futures = [
+                executor.submit(run_test, str(args.executable.absolute()), test_input) for test_input in test_inputs
+            ]
+            for future in as_completed(futures):
+                result, inputs, returncode = future.result()
 
-            # TODO: check the exit code of grepped
-            grepped = subprocess.run(
-                ["grep", "-E", "^Number of comparisons: [0-9]+$"], input=result.stdout, stdout=subprocess.PIPE,
-            )
+                match result:
+                    case ErrorCode.RETURNCODE_ERROR:
+                        print(f"Executable failed with {returncode} on input:\n{inputs}", file=sys.stderr)
+                        exit(1)
+                    case ErrorCode.NO_NUMBER_OF_COMPARISONS:
+                        print("Please modify your executable to count the amount of comparisons"
+                              "and print them in format: [Number of comparisons: -count-]", file=sys.stderr)
+                        exit(1)
+                    case _:
+                        results.append(result)
 
-            number_of_comparisons = int(grepped.stdout.decode("utf-8").strip("\n").split(" ")[3])
-            results.append(number_of_comparisons)
-        maximal_number_of_comparisons = calculate_number_of_maximal_comparisons(len(test_input))
+        maximal_number_of_comparisons = calculate_number_of_maximal_comparisons(test_set[1])
         print(f"Maximal comparisons allowed: {maximal_number_of_comparisons}")
         print(f"Worst result:                {max(*results)}")
         print(f"Best result:                 {min(*results)}")
-        print(f"Average result:              {sum(results) / len(results)}")
+        print(f"Average result:              {(sum(results) / len(results)):.1f}")
+
